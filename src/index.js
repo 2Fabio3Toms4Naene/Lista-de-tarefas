3dc93be0dceeaaa3905f64ef986a8ee5afa63833
Fabio Tomas Naene <fabiotomasnaene@gmail.com>
2026-01-15 12:00:21 +0200

    o arquivo importou a funcao para exibir o formulario para adicionar projetos

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../src/styles.css';
+import { handleDisplayForm } from './display-form';
+import { handleAddProjectToSidebar } from '../addProjectTitle';
+
+handleDisplayForm();
+handleAddProjectToSidebar();
