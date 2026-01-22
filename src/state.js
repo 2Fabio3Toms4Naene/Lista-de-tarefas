7987ff265e6901fd22ef74d5a5ce28a5be255177
Fabio Tomas Naene <fabiotomasnaene@gmail.com>
2026-01-22 11:18:29 +0200

    O estado da aplicacao foi definida

---
+++ src/state.js
@@ -0,0 +1,14 @@
+import { loadState, saveState } from "./storege";
+
+const savedState = loadState();
+
+export const state = savedState || {
+    activeProjectId: null,
+    editingTodoId: null,
+    formMode: null, // criar datalhes editar
+    projects: [],
+};
+
+export function updateState() {
+    saveState(state);
+};
